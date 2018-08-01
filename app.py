cc18bc6c3b9c02963448864b58a426a71e003f38
al.gomides@gmail.com <Alvaro Gomides>
2018-08-01 17:18:12 -0300

    Hello World do Flask com uma função de exemplo do COG

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from datetime import datetime
+import numpy as np
+from scipy.optimize import minimize
+from scipy.optimize import fmin
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+def totalcost(a):
+	
+    tc = np.sum(t*m*k*(((x-a[0])**2)+((y-a[1])**2))**0.5)
+    print("Custo = {}, X = {}, Y = {}".format(tc, a[0], a[1]))
+    return tc
+
+@app.route('/cog')
+def cog():
+	global t
+	global m
+	global k
+	global x
+	global y
+	x = [2, 5, 9, 7, 2]
+	x = np.array(x)
+	y = [1, 2, 1, 4, 5]
+	y = np.array(y)
+	m = [300, 500, 170, 120, 900]
+	m = np.array(m)
+	t = [0.002, 0.0015, 0.002, 0.0013, 0.0015]
+	t = np.array(t)
+	#Determining total costs
+	k = 50
+	b = []
+	a = [2, 0]
+	#print(totalcost(a))
+	#print(type(totalcost(a)))
+	sol = minimize(totalcost, (2,0), method="SLSQP", tol="0.1")
+	return """
+	A instalacao vai ser localizada nas seguintes coordenadas: x-> {} e y-> {}
+	""".format(sol.x[0], sol.x[1])
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
