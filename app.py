0f1c4a6374f43c985aba7157ba10f7caeee35f15
al.gomides@gmail.com <Alvaro Gomides>
2018-08-15 03:39:30 -0300

    Adicionando módulo de Requests

--- app.py
+++ app.py
@@ -19,10 +19,12 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
+
 def totalcost(a):
-    tc = np.sum(t*m*k*(((x-a[0])**2)+((y-a[1])**2))**0.5)
+    tc = np.sum(t * m * k * (((x - a[0]) ** 2) + ((y - a[1]) ** 2)) ** 0.5)
     print("Custo = {}, X = {}, Y = {}".format(tc, a[0], a[1]))
     return tc
+
 
 @app.route('/cog', methods=['POST'])
 def cog():
@@ -31,8 +33,11 @@ def cog():
     global k
     global x
     global y
-    #getting the parameters of the request
-    jdata = request.get_json()
+    # getting the parameters of the request
+    if (request.data):
+        jdata = request.get_json()
+    else:
+        return 'nothing to do here'
     x = []
     y = []
     m = []
@@ -42,25 +47,25 @@ def cog():
         y.extend(j[2])
         m.extend(j[3])
         t.extend(j[4])
-    #x = [2, 5, 9, 7, 2]
+    # x = [2, 5, 9, 7, 2]
     x = np.array(x)
-    #y = [1, 2, 1, 4, 5]
+    # y = [1, 2, 1, 4, 5]
     y = np.array(y)
-    #m = [300, 500, 170, 120, 900]
+    # m = [300, 500, 170, 120, 900]
     m = np.array(m)
-    #t = [0.002, 0.0015, 0.002, 0.0013, 0.0015]
+    # t = [0.002, 0.0015, 0.002, 0.0013, 0.0015]
     t = np.array(t)
-    #Determining total costs
+    # Determining total costs
     k = 50
     b = []
     a = [2, 0]
-    #print(totalcost(a))
-    #print(type(totalcost(a)))
-    sol = minimize(totalcost, (2,0), method="SLSQP", tol="0.1")
+    # print(totalcost(a))
+    # print(type(totalcost(a)))
+    sol = minimize(totalcost, (2, 0), method="SLSQP", tol="0.1")
     return """
     A instalacao vai ser localizada nas seguintes coordenadas: x-> {} e y-> {}
     """.format(sol.x[0], sol.x[1])
 
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
